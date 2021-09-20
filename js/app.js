46abd3388a58d9540d294af869e3f5a7d0c200fb
shaielgarcia <shaielgarcia@hotmail.com>
2021-09-20 20:00:21 -0300

    Realice operaciones en javascript

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// Operaciones
+let nombre = "Shaiel"
+let apellido = "Garcia"
+
+console.log(`${nombre} ${apellido}`);
+
+
+let num1 = Number(prompt("Ingresa tu edad"));
+let num2 = Number(prompt("Ingresa tu año de nacimiento"));
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
