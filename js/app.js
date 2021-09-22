fa6dcacb8e49939fc3cfb952e2363539fb4008a8
shaielgarcia <shaielgarcia@hotmail.com>
2021-09-22 19:51:25 -0300

    agregue ciclos con while en mi pagina

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
-// algoritmo con un condicional
+let contraseña = 52511
+let pregunta = Number(prompt("Ingresar contraseña"))
+intentos = 3
 
-const nom1 = "shaiel1"
-const nom2 = "shai02"
-const nom3 = "shaii2"
+while (pregunta != contraseña && intentos > 0) {
+    alert(`te equivocaste te quedan ${intentos} intentos`);
+    intentos--
+    dato = Number(prompt("Ingresar contraseña"));
 
-let pregunta = prompt("Ingrese tu usuario")
-
-pregunta = pregunta.toLowerCase()
-
-if (pregunta === nom1 || pregunta === nom2 || pregunta === nom3) console.log(` Podes ingresar ${pregunta}`);
-else { alert(`Nombre de usuario incorrecto ${pregunta} `) }
+    let validar = confirm("Desea cambiar la contraseña?")
+    if (validar) {
+        contraseña = Number(prompt("Ingresa tu nueva contraseña"));
+    }
+}
