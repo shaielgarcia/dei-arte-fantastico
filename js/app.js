9802490a2c58f2fc8bc3968493ab5170d232be6c
shaielgarcia <shaielgarcia@hotmail.com>
2021-10-06 22:59:08 -0300

    Realice arrays en mi java script

--- js/app.js
+++ js/app.js
@@ -1,146 +1,75 @@
-function Seguro(marca, anio, tipo) {
-    this.marca = marca;
-    this.anio = anio;
-    this.tipo = tipo;
+//Array
+let obras = ["buda", "fauno", "duende del jardin", "criatura mitologica", "hadas", "dragones", "unicornio", "magos"]
+
+console.log(obras);
+
+obras.push("c magica")
+console.log(obras);
+
+obras.push("ogros")
+console.log(obras);
+
+obras.unshift("duende");
+console.log(obras);
+
+obras[3] = "Elementales"
+console.log(obras);
+
+obras.sort()
+console.log(obras)
+
+let listaDefinitiva = obras.concat(obras)
+console.log(listaDefinitiva);
+
+let listaReducida = listaDefinitiva.slice(2, 8)
+console.log(listaReducida)
+
+let lista1 = listaDefinitiva.slice(0, 3)
+let lista2 = listaDefinitiva.slice(8, listaDefinitiva.length)
+let lista3 = lista1.concat(lista2)
+console.log(lista3)
+
+class esculturas {
+    constructor(nombre, precio) {
+        this.nombre = nombre
+        this.precio = precio
+    }
 }
 
-Seguro.prototype.cotizarSeguro = function() {
+let listaObras = [];
 
-    let cantidad;
-    const base = 2000;
+listaObras.push(new esculturas("Duende del jardin", 7000))
+listaObras.push(new esculturas("Buda", 9000))
+listaObras.push(new esculturas("Fauno", 8000))
+listaObras.push(new esculturas("Hadas", 2000))
+listaObras.push(new esculturas("Elemental", 6000))
+listaObras.push(new esculturas("Duende de cometa", 7000))
 
-    switch (this.marca) {
-        case '1':
-            cantidad = base * 1.15;
-            break;
-        case '2':
-            cantidad = base * 1.05;
-            break;
-        case '3':
-            cantidad = base * 1.35;
-            break;
-    }
-
-    const diferencia = new Date().getFullYear() - this.anio;
-
-    cantidad -= ((diferencia * 3) * cantidad) / 100;
-
-    if (this.tipo === 'basico') {
-        cantidad *= 1.30;
-    } else {
-        cantidad *= 1.50;
-    }
-    return cantidad;
+const agregarEsculturasALaLista = () => {
+    let nombre = prompt("Nombre de la escultura");
+    let precio = Number(prompt("Precio del producto"));
+    let esculturas = new esculturas(nombre, precio)
+    listaObras.push(esculturas)
 }
 
-
-function Interfaz() {}
-
-
-Interfaz.prototype.mostarMensaje = function(mensaje, tipo) {
-    const div = document.createElement("div");
-
-    if (tipo === 'error') {
-        div.classList.add('mensaje', 'error');
-    } else {
-        div.classList.add("mensaje", "correcto");
+listaObras.sort((a, b) => {
+    if (a.precio > b.precio) {
+        return 1
     }
 
-    div.innerHTML = `${mensaje}`;
-    formulario.insertBefore(div, document.querySelector(".form-group"));
-
-    setTimeout(function() {
-        document.querySelector('.mensaje').remove();
-    }, 2000);
-};
-
-Interfaz.prototype.mostrarResultado = function(seguro, total) {
-    const resultado = document.getElementById('resultado');
-    let marca;
-
-    switch (seguro.marca) {
-        case '1':
-            marca = 'Americano';
-            break;
-        case '2':
-            marca = 'Asiático';
-            break;
-        case '3':
-            marca = 'Europeo';
-            break;
+    if (a.precio < b.precio) {
+        return -1
     }
+    return 0
+})
 
+listaObras.toString()
+console.log(listaObras);
 
-    const div = document.createElement('div');
+listaObras.forEach(obj => {
+    console.log(obj.precio);
+})
 
-    div.innerHTML = `
-       <p class="header">Tu resumen:</p>
-       <p>Marca: ${marca}</p>
-       <p> Año: ${seguro.anio}</p>
-       <p>Tipo: ${seguro.tipo}</p>
-       <p>Total: $ ${total}</p>   
-    `;
-    const spinner = document.querySelector('#cargando img');
-    spinner.style.display = 'block';
-
-    setTimeout(function() {
-        spinner.style.display = 'none';
-        resultado.appendChild(div);
-    }, 500);
-
-}
-
-
-const formulario = document.getElementById('cotizar-seguro');
-
-formulario.addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    const marca = document.getElementById('marca');
-    const marcaSeleccionada = marca.options[marca.selectedIndex].value;
-
-    const anio = document.getElementById("anio");
-    const anioSeleccionado = anio.options[anio.selectedIndex].value;
-
-    const tipo = document.querySelector('input[name="tipo"]:checked').value;
-
-
-    const interfaz = new Interfaz();
-
-
-    if (marcaSeleccionada === '' || anioSeleccionado === '' || tipo === '') {
-
-        interfaz.mostarMensaje('Faltan Datos, revisa e intenta de nuevo', 'error');
-    } else {
-
-        const resultados = document.querySelector('#resultado div');
-        if (resultados != null) {
-            resultados.remove();
-        }
-
-        const seguro = new Seguro(marcaSeleccionada, anioSeleccionado, tipo);
-
-        const cantidad = seguro.cotizarSeguro(seguro);
-
-        interfaz.mostrarResultado(seguro, cantidad);
-        interfaz.mostarMensaje('Cotizando', 'correcto');
-
-    }
-
-});
-
-
-
-
-const max = new Date().getFullYear(),
-    min = max - 20;
-
-const selectAnios = document.getElementById('anio');
-
-for (let i = max; i > min; i--) {
-    let option = document.createElement('option');
-    option.value = i;
-    option.innerHTML = i;
-    selectAnios.appendChild(option);
-
-}
+let search = prompt("Que obra buscas?")
+let buscadorMultiple = listaObras.filter(obj => obj.nombre === search)
+console.log(buscadorMultiple);
